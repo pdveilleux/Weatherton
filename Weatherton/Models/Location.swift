c01679f96c69d892b1b078c459025bd24758798a
Patrick Veilleux <pdveilleux.dev@gmail.com>
2024-09-01 20:53:16 -0500

    Move inline location definitions to static vars

--- Weatherton/Models/Location.swift
+++ Weatherton/Models/Location.swift
@@ -25,3 +25,48 @@ extension Location: Equatable {
         lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
 }
+
+extension Location {
+    static let minneapolis = Location(
+        name: "Minneapolis",
+        region: "Minnesota",
+        country: "United States of America",
+        latitude: 44.98,
+        longitude: -93.26
+    )
+    static let london = Location(
+        name: "London",
+        region: "City of London, Greater London",
+        country: "United Kingdom",
+        latitude: 51.52,
+        longitude: -0.11
+    )
+    static let tokyo = Location(
+        name: "Tokyo",
+        region: "Tokyo",
+        country: "Japan",
+        latitude: 35.69,
+        longitude: 139.69
+    )
+    static let saoPaulo = Location(
+        name: "Sao Paulo",
+        region: "Sao Paulo",
+        country: "Brazil",
+        latitude: -23.53,
+        longitude: -46.62
+    )
+    static let nairobi = Location(
+        name: "Nairobi",
+        region: "Nairobi Area",
+        country: "Kenya",
+        latitude: -1.28,
+        longitude: 36.82
+    )
+    static let hongKong = Location(
+        name: "Hong Kong",
+        region: "",
+        country: "Hong Kong",
+        latitude: 22.28,
+        longitude: 114.15
+    )
+}
